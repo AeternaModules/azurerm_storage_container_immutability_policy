82ed6c290b8d71bf65797ccbb6416781147f0fef
Daniel Allen <dan@aeternagroup.net>
2026-01-11 19:29:01 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "storage_container_immutability_policys" {
+  description = "All storage_container_immutability_policy resources"
+  value       = azurerm_storage_container_immutability_policy.storage_container_immutability_policys
+}
+
