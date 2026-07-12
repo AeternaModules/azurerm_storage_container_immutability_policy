e42fea6f180b9c59860cd8622ad103f9ec1dc8e6
Daniel Allen <dan@aeternagroup.net>
2026-07-12 16:32:02 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "storage_container_immutability_policies_id" {
+  description = "Map of id values across all storage_container_immutability_policies, keyed the same as var.storage_container_immutability_policies"
+  value       = { for k, v in azurerm_storage_container_immutability_policy.storage_container_immutability_policies : k => v.id }
+}
 output "storage_container_immutability_policies_immutability_period_in_days" {
   description = "Map of immutability_period_in_days values across all storage_container_immutability_policies, keyed the same as var.storage_container_immutability_policies"
   value       = { for k, v in azurerm_storage_container_immutability_policy.storage_container_immutability_policies : k => v.immutability_period_in_days }
