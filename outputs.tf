b2650aefe98cf5ba2bf91d2bdbf65696d177001e
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:20:47 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,25 +1,21 @@
-output "storage_container_immutability_policies" {
-  description = "All storage_container_immutability_policy resources"
-  value       = azurerm_storage_container_immutability_policy.storage_container_immutability_policies
-}
 output "storage_container_immutability_policies_immutability_period_in_days" {
-  description = "List of immutability_period_in_days values across all storage_container_immutability_policies"
-  value       = [for k, v in azurerm_storage_container_immutability_policy.storage_container_immutability_policies : v.immutability_period_in_days]
+  description = "Map of immutability_period_in_days values across all storage_container_immutability_policies, keyed the same as var.storage_container_immutability_policies"
+  value       = { for k, v in azurerm_storage_container_immutability_policy.storage_container_immutability_policies : k => v.immutability_period_in_days }
 }
 output "storage_container_immutability_policies_locked" {
-  description = "List of locked values across all storage_container_immutability_policies"
-  value       = [for k, v in azurerm_storage_container_immutability_policy.storage_container_immutability_policies : v.locked]
+  description = "Map of locked values across all storage_container_immutability_policies, keyed the same as var.storage_container_immutability_policies"
+  value       = { for k, v in azurerm_storage_container_immutability_policy.storage_container_immutability_policies : k => v.locked }
 }
 output "storage_container_immutability_policies_protected_append_writes_all_enabled" {
-  description = "List of protected_append_writes_all_enabled values across all storage_container_immutability_policies"
-  value       = [for k, v in azurerm_storage_container_immutability_policy.storage_container_immutability_policies : v.protected_append_writes_all_enabled]
+  description = "Map of protected_append_writes_all_enabled values across all storage_container_immutability_policies, keyed the same as var.storage_container_immutability_policies"
+  value       = { for k, v in azurerm_storage_container_immutability_policy.storage_container_immutability_policies : k => v.protected_append_writes_all_enabled }
 }
 output "storage_container_immutability_policies_protected_append_writes_enabled" {
-  description = "List of protected_append_writes_enabled values across all storage_container_immutability_policies"
-  value       = [for k, v in azurerm_storage_container_immutability_policy.storage_container_immutability_policies : v.protected_append_writes_enabled]
+  description = "Map of protected_append_writes_enabled values across all storage_container_immutability_policies, keyed the same as var.storage_container_immutability_policies"
+  value       = { for k, v in azurerm_storage_container_immutability_policy.storage_container_immutability_policies : k => v.protected_append_writes_enabled }
 }
 output "storage_container_immutability_policies_storage_container_resource_manager_id" {
-  description = "List of storage_container_resource_manager_id values across all storage_container_immutability_policies"
-  value       = [for k, v in azurerm_storage_container_immutability_policy.storage_container_immutability_policies : v.storage_container_resource_manager_id]
+  description = "Map of storage_container_resource_manager_id values across all storage_container_immutability_policies, keyed the same as var.storage_container_immutability_policies"
+  value       = { for k, v in azurerm_storage_container_immutability_policy.storage_container_immutability_policies : k => v.storage_container_resource_manager_id }
 }
 
